717fe3da8b890066da62a93b0d2ce51c01f1e87f
Matt DeVoe <matthew.devoe@gmail.com>
2018-05-15 14:18:29 -0600

    installed vuex, setup base state/store

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -5,23 +5,18 @@
 </template>
 
 <script>
-import Stock from './Stock.vue'
+  import Stock from './Stock.vue'
 
-export default {
-  components: {
-    appStock: Stock
-  },
-  data() {
-    return {
-      stocks: [
-        { id: 1, name: 'BMW', price: 110 },
-        { id: 2, name: 'Apple', price: 210 },
-        { id: 3, name: 'Google', price: 90 },
-        { id: 4, name: 'Twitter', price: 150 },
-      ]
+  export default {
+    components: {
+      appStock: Stock
+    },
+    computed: {
+      stocks() {
+        return this.$store.getters.stocks
+      }
     }
   }
-}
 </script>
 
 <style lang="css">
