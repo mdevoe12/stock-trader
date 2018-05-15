f0a9417dc672af0472c4613b473ba22a9f1ccc54
Matt DeVoe <matthew.devoe@gmail.com>
2018-05-15 12:51:35 -0600

    added stockin info to stock component and stocks

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,26 @@
 <template lang="html">
-  <h1>Stocks/Stocks Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks"></app-stock>
+  </div>
 </template>
 
 <script>
+import Stock from './Stock.vue'
+
 export default {
+  components: {
+    appStock: Stock
+  },
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 110 },
+        { id: 2, name: 'Apple', price: 210 },
+        { id: 3, name: 'Google', price: 90 },
+        { id: 4, name: 'Twitter', price: 150 },
+      ]
+    }
+  }
 }
 </script>
 
